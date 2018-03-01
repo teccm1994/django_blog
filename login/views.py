26df36a7db1eef605c022341dd1ebc7187351d29
chenming <chenming@xjgreat.com>
2018-03-01 10:59:29 +0800

    add register and hashlib

--- login/views.py
+++ login/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from login.models import User
-from django import forms
-from .forms import UserForm
+from .forms import UserForm, RegisterForm
+import hashlib
 
 # Create your views here.
 def index(request):
@@ -9,7 +9,7 @@ def index(request):
     return render(request, 'login/index.html')
 
 def login(request):
-    if request.session.get('is_login',None):
+    if request.session.get('is_login', None):
         return redirect("/index/")
     if request.method == "POST":
         login_form = UserForm(request.POST)
@@ -19,7 +19,7 @@ def login(request):
             password = login_form.cleaned_data['password']
             try:
                 user = User.objects.get(name=username)
-                if user.password == password:
+                if user.password == hash_code(password):
                     request.session['is_login'] = True
                     request.session['user_id'] = user.id
                     request.session['user_name'] = user.name
@@ -34,8 +34,42 @@ def login(request):
     return render(request, 'login/login.html', locals())
 
 def register(request):
-    pass
-    return render(request, 'login/register.html')
+    if request.session.get('is_login', None):
+        # 登录状态不允许注册。你可以修改这条原则！
+        return redirect("/index/")
+    if request.method == "POST":
+        register_form = RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():  # 获取数据
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            sex = register_form.cleaned_data['sex']
+            if password1 != password2:  # 判断两次密码是否相同
+                message = "两次输入的密码不同！"
+                return render(request, 'login/register.html', locals())
+            else:
+                same_name_user = User.objects.filter(name=username)
+                if same_name_user:  # 用户名唯一
+                    message = '用户已经存在，请重新选择用户名！'
+                    return render(request, 'login/register.html', locals())
+                same_email_user = User.objects.filter(email=email)
+                if same_email_user:  # 邮箱地址唯一
+                    message = '该邮箱地址已被注册，请使用别的邮箱！'
+                    return render(request, 'login/register.html', locals())
+
+                # 当一切都OK的情况下，创建新用户
+
+                new_user = User.objects.create()
+                new_user.name = username
+                new_user.password = hash_code(password1)  # 使用加密密码
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+                return redirect('/login/')  # 自动跳转到登录页面
+    register_form = RegisterForm()
+    return render(request, 'login/register.html', locals())
 
 def logout(request):
     if not request.session.get('is_login', None):
@@ -47,3 +81,9 @@ def logout(request):
     # del request.session['user_id']
     # del request.session['user_name']
     return redirect("/index/")
+
+def hash_code(s, salt='my_blog'):# 加点盐
+    h = hashlib.sha256()
+    s += salt
+    h.update(s.encode())  # update方法只接收bytes类型
+    return h.hexdigest()
